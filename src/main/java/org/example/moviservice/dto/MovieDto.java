a14aa767872b966aaf8b408049effc52c316e00b
Максентий <113115807+Maksentiu@users.noreply.github.com>
2024-03-05 02:17:38 +0300

    MovieDto.java

--- src/main/java/org/example/moviservice/dto/MovieDto.java
+++ src/main/java/org/example/moviservice/dto/MovieDto.java
@@ -3,7 +3,9 @@ package org.example.moviservice.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.Setter;
 
+@Setter
 @Getter
 public class MovieDto {
     @JsonProperty
