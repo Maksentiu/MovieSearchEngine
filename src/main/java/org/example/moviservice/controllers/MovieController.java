6453ec1bcee5af7fbd0f58631240944421ce5f7d
Максентий <113115807+Maksentiu@users.noreply.github.com>
2024-04-11 11:28:18 +0300

    MovieController.java

--- src/main/java/org/example/moviservice/controllers/MovieController.java
+++ src/main/java/org/example/moviservice/controllers/MovieController.java
@@ -1,14 +1,18 @@
 package org.example.moviservice.controllers;
 
 import lombok.AllArgsConstructor;
-import org.example.moviservice.cache.MovieCache;
 import org.example.moviservice.dto.MovieDto;
-import org.example.moviservice.services.CommentService;
 import org.example.moviservice.services.MovieService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @AllArgsConstructor
